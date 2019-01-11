c478e8a1203c9e9a172f0e209f6bbd58d50f651b
Alexey Piskarev <stromboli@yandex.ru>
2019-01-11 18:21:16 +0300

    Added timeout detection on client side

--- WebApp/ClientApp/src/app/fetch-data/fetch-data.component.ts
+++ WebApp/ClientApp/src/app/fetch-data/fetch-data.component.ts
@@ -7,11 +7,30 @@ import { HttpClient } from '@angular/common/http';
 })
 export class FetchDataComponent {
   public forecasts: WeatherForecast[];
+  public error: any;
 
-  constructor(http: HttpClient, @Inject('BASE_URL') baseUrl: string) {
-    http.get<WeatherForecast[]>(baseUrl + 'api/SampleData/WeatherForecasts').subscribe(result => {
-      this.forecasts = result;
-    }, error => console.error(error));
+  constructor(http: HttpClient, @Inject('BASE_URL') baseUrl: string)
+  {
+    var TimeoutMs = 6000;
+    var TimeIsUp = true;
+
+    new Promise<WeatherForecast[]> ((resolve, reject) =>
+    {
+      setTimeout(() => TimeIsUp && reject (new Error ("timeout")), TimeoutMs);
+
+      http.get<WeatherForecast[]>(baseUrl + 'api/SampleData/WeatherForecasts')
+        .subscribe(
+          result => resolve(result),
+          error => reject(error)
+        );
+    })
+    .then(result =>
+      {
+        TimeIsUp = false;
+        this.forecasts = result;
+      })
+    .catch (error => console.error(error))
+    ;
   }
 }
 
