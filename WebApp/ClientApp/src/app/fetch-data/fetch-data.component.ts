074929e91b9be15b6d90eeae33e4db9582d9588e
Alexey Piskarev <stromboli@yandex.ru>
2019-01-12 21:02:41 +0300

    Added countdown to retrial

--- WebApp/ClientApp/src/app/fetch-data/fetch-data.component.ts
+++ WebApp/ClientApp/src/app/fetch-data/fetch-data.component.ts
@@ -6,22 +6,31 @@ import { FetchWithTimeout } from '../fetchwithtimeout';
   selector: 'app-fetch-data',
   templateUrl: './fetch-data.component.html'
 })
-export class FetchDataComponent {
+export class FetchDataComponent
+{
+  http: HttpClient;
+  baseUrl: string;
+
   public forecasts: WeatherForecast[];
   public error: any;
+  public retryTime: Date;
+  public secondsToRetry: number;
 
   constructor(http: HttpClient, @Inject('BASE_URL') baseUrl: string)
   {
-    this.TryFetchData(http, baseUrl);
+    this.http = http;
+    this.baseUrl = baseUrl;
+
+    this.TryFetchData();
   }
 
-  TryFetchData(http: HttpClient, baseUrl: string): void
+  TryFetchData(): void
   {
     var timeoutMs = 6000;
 
     FetchWithTimeout<WeatherForecast[]> (
-        http,
-        baseUrl + 'api/SampleData/WeatherForecasts',
+        this.http,
+        this.baseUrl + 'api/SampleData/WeatherForecasts',
         timeoutMs
       )
       .then (result =>
@@ -35,12 +44,32 @@ export class FetchDataComponent {
           this.forecasts = null;
           this.error = error;
 
-          setTimeout(() =>
-          {
-            this.TryFetchData(http, baseUrl);
-          }, 3000);
+          this.retryTime = new Date();
+          this.retryTime.setSeconds(this.retryTime.getSeconds() + 5);
+
+          setTimeout(() => this.WaitingForRetry(), 0);
         })
       ;
+  }
+
+  WaitingForRetry()
+  {
+    var dt = new Date();
+
+    if (dt >= this.retryTime)
+    {
+      this.retryTime = null;
+      this.secondsToRetry = null;
+      this.error = null;
+
+      setTimeout(() => this.TryFetchData(), 0);
+
+      return;
+    }
+
+    this.secondsToRetry = Math.floor((<any>this.retryTime - <any>dt) / 1000);
+
+    setTimeout(() => this.WaitingForRetry(), 1000);
   }
 }
 
